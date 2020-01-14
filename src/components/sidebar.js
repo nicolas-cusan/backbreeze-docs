17f1ed4601763b53ca8c4beb1304b42008c499b7
Nicolas Cusan <nc@arillo.ch>
2020-01-15 00:34:02 +0100

    add trailing slash to sidebar links

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -80,7 +80,7 @@ export default () => (
               <ul>
                 {docs[item].map(i => (
                   <li key={i.id}>
-                    <SidebarLink to={`/${i.name}`} title={i.name} />
+                    <SidebarLink to={`/${i.name}/`} title={i.name} />
                   </li>
                 ))}
               </ul>
