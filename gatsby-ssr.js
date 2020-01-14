0cf511c404ed19d4759238b2f8402942a46b461d
Nicolas Cusan <nc@arillo.ch>
2020-01-14 23:34:27 +0100

    add ssr wrapPageElement

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react';
+import LayoutWrap from 'components/layout-wrap';
+
+export const wrapPageElement = ({ element, props }) => (
+  <LayoutWrap {...props}>{element}</LayoutWrap>
+);
