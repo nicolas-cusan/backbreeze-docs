5829bf71d42306fe1e97dd54b13f6a3f287669eb
Nicolas Cusan <nc@arillo.ch>
2020-04-24 12:35:59 +0200

    Improve 404 route

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import SEO from 'components/seo';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1 className="-ls-sm">NOT FOUND</h1>
+    <h1 className="fs-34 lh-1.2 fw-500 -ls-sm mb-x1">404 — Not found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 );
